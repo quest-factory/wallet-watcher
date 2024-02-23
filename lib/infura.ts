c318a1aecc61086c577b210ed7bf68c5bb2e8a47
Franck Fernandez <franck.fernandez@gmail.com>
2024-02-23 16:19:58 +0100

    Wrap getBalance() in trycatch

--- lib/infura.ts
+++ lib/infura.ts
@@ -5,20 +5,24 @@ const infuraHeaders = {
 };
 
 export async function getBalance(addr: string) {
-  const response = await fetch(
-    `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-    {
-      headers: infuraHeaders,
-      method: 'POST',
-      body: JSON.stringify({
-        jsonrpc: '2.0',
-        id: 1,
-        method: 'eth_getBalance',
-        params: [addr, 'latest'],
-      }),
-    }
-  );
-  const { result } = await response.json();
+  try {
+    const response = await fetch(
+      `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      {
+        headers: infuraHeaders,
+        method: 'POST',
+        body: JSON.stringify({
+          jsonrpc: '2.0',
+          id: 1,
+          method: 'eth_getBalance',
+          params: [addr, 'latest'],
+        }),
+      }
+    );
+    const { result } = await response.json();
 
-  return hexToDecimals(result);
+    return hexToDecimals(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
