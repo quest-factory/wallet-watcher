7d4a39fc36576a5adb4c60fb05b5cd6fe0b93e2d
Baptiste <avocalam@gmail.com>
2024-02-23 15:12:01 +0100

    add navbar to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@nextui-org/button";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex  flex-col items-center justify-center">
+      <NavBar />
       <Button>wallet watcher</Button>
     </main>
   );
