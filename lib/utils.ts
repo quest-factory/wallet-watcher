34e247b4d82065baeb1c5b33b9bd7f2d8580d76f
Franck Fernandez <franck.fernandez@gmail.com>
2024-02-23 18:10:27 +0100

    Add getCurrencyValue() utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,2 +1,5 @@
 export const hexToDecimals = (value: string, decimals: number = 18) =>
   parseInt(value, 16) / Math.pow(10, decimals);
+
+export const getCurrencyValue = (v: number) =>
+  v < 0.1 ? `$${v}` : `$${new Intl.NumberFormat('en-US').format(v)}`;
