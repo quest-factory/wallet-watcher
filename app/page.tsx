1e173b70cbabd3c7b3e4951b7bd14829d14708be
Baptiste <avocalam@gmail.com>
2024-02-26 11:52:40 +0100

    add usage of local storage wallets

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import AccountCard from '@/components/AccountCard';
+'use client'
 
-export default async function Home() {
-  const ACCOUNTS = [
-    { name: 'John', address: '0xDAFEA492D9c6733ae3d56b7Ed1ADB60692c98Bc5' },
-    { name: 'Marie', address: '0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5' },
-    { name: 'Tod', address: '0xc964D6AbAF8648642d984fb70C295b8e49bB4AF6' },
-  ];
+import AccountCard from '@/components/AccountCard';
+import useLocalStorage from '@/lib/useLocalStorage';
+
+export default function Home() {
+  const { walletsLocal } = useLocalStorage();
 
   return (
     <main className="w-full mx-auto max-w-[80%] mt-10">
@@ -13,9 +12,9 @@ export default async function Home() {
         <h1>Accounts</h1>
 
         <div className="grid lg:grid-cols-2 2xl:grid-cols-3 gap-3">
-          {ACCOUNTS.sort().map((account) => (
+          {walletsLocal.sort().map((account, index) => (
             <AccountCard
-              key={account.address}
+              key={index}
               className="mx-auto"
               {...account}
             />
