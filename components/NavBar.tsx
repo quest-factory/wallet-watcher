849966680ac546a6aa015e923603433f286e8371
Baptiste <avocalam@gmail.com>
2024-02-27 16:40:40 +0100

    add link to the brand

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,12 +5,16 @@ import {
   NavbarItem,
 } from '@nextui-org/react';
 import AddWalletModal from './AddWalletModal';
+import Link from 'next/link';
 
 export default function NavBar() {
   return (
-    <Navbar classNames={{ wrapper: 'max-w-none' }} isBordered>
+    <Navbar isBordered>
       <NavbarBrand>
-        <p className="font-bold text-inherit">Wallet Watcher</p>
+        <Link href="/" className="flex">
+          <p className="font-bold text-inherit">Wallet </p>
+          <p className="font-bold text-inherit text-secondary">Watcher</p>
+        </Link>
       </NavbarBrand>
 
       <NavbarContent justify="end">
