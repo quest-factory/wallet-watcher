fea26a755c910545ea276155b88a8a050a0c5a85
Franck Fernandez <franck.fernandez@gmail.com>
2024-02-28 17:05:32 +0100

    Create getAddresses(), addAddresses() and addAddressesSubmit() server actions

---
+++ actions/addresses.ts
@@ -0,0 +1,67 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import { Tables } from '../types_db';
+import { createClient } from '@/lib/supabase/server';
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+
+export async function getAddresses(): Promise<Tables<'addresses'> | null> {
+  const supabase = createClient(cookies());
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect('/');
+  }
+
+  const { data, error } = await supabase
+    .from('addresses')
+    .select('*')
+    .eq('user_id', session.user.id)
+    .order('id', { ascending: true });
+
+  if (error) {
+    console.log(error.message);
+  }
+
+  return data as Tables<'addresses'> | null;
+}
+
+export async function addAddresses({
+  address,
+  name,
+}: Omit<Tables<'addresses'>, 'id' | 'user_id'>) {
+  const supabase = createClient(cookies());
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect('/');
+  }
+
+  const { statusText, error } = await supabase
+    .from('addresses')
+    .insert({ address, name, user_id: session.user.id });
+
+  if (error) {
+    console.log(error.message);
+  }
+
+  revalidatePath('/');
+  return statusText;
+}
+
+export async function addAddressesSubmit(_: any, formData: FormData) {
+  const address = formData.get('address')?.toString();
+  const name = formData.get('name')?.toString();
+
+  if (!name || !address) {
+    return;
+  }
+
+  const statusText = await addAddresses({ address, name });
+  return { message: statusText };
+}
--- actions/users.ts
+++ actions/users.ts
@@ -1,3 +1,5 @@
+'use server';
+
 import { cookies } from 'next/headers';
 import { createClient } from '@/lib/supabase/server';
 
