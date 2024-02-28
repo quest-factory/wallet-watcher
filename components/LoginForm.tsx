3fcffec31d4fd4d484ba159faeb8d1b72c190705
Franck Fernandez <franck.fernandez@gmail.com>
2024-02-28 11:45:56 +0100

    Create Login form

---
+++ components/LoginForm.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { Card, CardHeader, Divider, CardBody } from '@nextui-org/react';
+
+export default function LoginForm({ className = '' }: { className?: string }) {
+  const supabaseClient = useSupabaseClient();
+
+  return (
+    <Card className={`${className} max-w-[450px] p-5`}>
+      <CardHeader className="flex gap-3">
+        <h2 className="text-md">Login</h2>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <Auth
+          supabaseClient={supabaseClient}
+          providers={[]}
+          magicLink
+          appearance={{
+            theme: ThemeSupa,
+            variables: {
+              default: {
+                colors: {
+                  brand: '#7828C8',
+                  brandAccent: '#7828C8',
+                },
+                radii: {
+                  borderRadiusButton: '12px',
+                  buttonBorderRadius: '12px',
+                  inputBorderRadius: '12px',
+                },
+              },
+            },
+          }}
+        />
+      </CardBody>
+    </Card>
+  );
+}
