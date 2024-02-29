afa230d757b2e5281618b22e4a625cf48c7eea60
Baptiste <avocalam@gmail.com>
2024-02-29 15:22:25 +0100

    add user mail and btn disconnect display

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,4 +1,7 @@
 import {
+  Avatar,
+  Button,
+  Chip,
   Navbar,
   NavbarBrand,
   NavbarContent,
@@ -6,8 +9,17 @@ import {
 } from '@nextui-org/react';
 import AddWalletModal from './AddWalletModal';
 import Link from 'next/link';
+import { createClient } from '@/lib/supabase/server';
+import { cookies } from 'next/headers';
 
-export default function NavBar() {
+export default async function NavBar() {
+  const supabase = createClient(cookies());
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  console.log('session : ', session?.user.email);
+
   return (
     <Navbar isBordered>
       <NavbarBrand>
@@ -18,9 +30,22 @@ export default function NavBar() {
       </NavbarBrand>
 
       <NavbarContent justify="end">
-        <NavbarItem>
-          <AddWalletModal />
-        </NavbarItem>
+        {session?.user.email ? (
+          <>
+            <NavbarItem>
+              <AddWalletModal />
+            </NavbarItem>
+            <NavbarItem className="flex justify-end items-center gap-3">
+              <div className="flex flex-col justify-center items-end text-xs gap-1">
+                <p className="font-bold">{session?.user.email}</p>
+                <Button className="h-4 text-xs">Disconnect</Button>
+              </div>
+              <Avatar name={session?.user.email} color="secondary" />
+            </NavbarItem>
+          </>
+        ) : (
+          <NavbarItem>Not connected</NavbarItem>
+        )}
       </NavbarContent>
     </Navbar>
   );
