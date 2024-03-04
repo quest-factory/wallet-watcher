0c5eb662d73362d98b94e6aaf533ffe60fab1851
Baptiste <avocalam@gmail.com>
2024-03-04 17:06:27 +0100

    now handle page

--- lib/etherscan.ts
+++ lib/etherscan.ts
@@ -1,7 +1,7 @@
-export async function getTransactions(address: string) {
+export async function getTransactions(address: string, page: string) {
   try {
     const response = await fetch(
-      `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=${process.env.ETHERSCAN_KEY}`
+      `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=${page}&offset=20&sort=desc&apikey=${process.env.ETHERSCAN_KEY}`
     );
 
     if (!response.ok) {
