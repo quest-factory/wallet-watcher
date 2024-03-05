9dabb3f71e2f9d0f87252329aa9fec8332d64279
Baptiste <avocalam@gmail.com>
2024-03-05 11:40:34 +0100

    display wallet name in title

--- app/wallet-details/[address]/page.tsx
+++ app/wallet-details/[address]/page.tsx
@@ -1,19 +1,67 @@
-import { Snippet } from '@nextui-org/react';
+import {
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  Chip,
+  Snippet,
+} from '@nextui-org/react';
 import TransactionsTable from './component/TransactionTable';
 import { Suspense } from 'react';
 import BalanceSnippet from './component/BalanceSnippet';
 import BalanceSnippetSkeleton from './component/BalanceSnippetSkeleton';
+import { getAddresses } from '@/lib/queries/addresses';
+import AddWalletModal from '@/components/AddWalletModal';
 
 export default async function WalletDetails({
   params,
 }: {
   params: { address: string };
 }) {
+  const wallets = await getAddresses();
+
+  const getCurrentWallet = () => {
+    if (wallets) {
+      return wallets.filter(
+        (wallets) =>
+          wallets.address.toLocaleLowerCase() ===
+          params.address.toLocaleLowerCase()
+      );
+    }
+    return [];
+  };
+
   return (
     <main className="w-full mx-auto max-w-[80%] my-10">
       <section className="space-y-12 flex flex-col items-center">
-        <h2>Wallet details</h2>
-
+        <div className="flex w-full justify-between px-5">
+          <h3 className="w-full flex flex-col items-center justify-center">
+            <p>Wallet details</p>
+            <div className="flex gap-5 justify-center items-center">
+              {getCurrentWallet().length > 0 ? (
+                <>
+                  {getCurrentWallet().map((elem, index) => (
+                    <p className="text-secondary font-bold">{elem.name}</p>
+                  ))}
+                </>
+              ) : (
+                <Card className="h-7 mt-5">
+                  <CardBody className="text-sm flex flex-row items-center justify-center gap-3 py-2">
+                    <p className="text-gray-600">
+                      This wallet is not in your saved accounts.
+                    </p>
+                    {/* <Button size="sm" color="secondary" className="h-5">Add this wallet</Button> */}
+                    <AddWalletModal
+                      customText="Add this wallet"
+                      addressPreset={params.address}
+                      btnClassName="h-5"
+                    />
+                  </CardBody>
+                </Card>
+              )}
+            </div>
+          </h3>
+        </div>
         <div className="flex w-full justify-center gap-5">
           <div className="flex flex-col justify-items-center w-full text-center">
             <p className="font-bold">Address</p>
@@ -33,7 +81,7 @@ export default async function WalletDetails({
         </div>
 
         <div className="flex flex-col gap-3 w-full">
-          <TransactionsTable address={params.address} />
+          <TransactionsTable address={params.address} wallets={wallets} />
         </div>
       </section>
     </main>
