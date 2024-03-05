a23773dfe05932b5795b4bbb2305f3452cd195d2
Baptiste <avocalam@gmail.com>
2024-03-05 16:36:38 +0100

    put space between value and $

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@ export const hexToDecimals = (value: string, decimals: number = 18) =>
   parseInt(value, 16) / Math.pow(10, decimals);
 
 export const getCurrencyValue = (v: number) =>
-  v < 0.1 ? `$${v}` : `$${new Intl.NumberFormat('en-US').format(v)}`;
+  v < 0.1 ? `$ ${v}` : `$ ${new Intl.NumberFormat('en-US').format(v)}`;
 
 export const strWeiToStrEth = (weiString: string) => {
   const weiLength = weiString.length;
