72224b6ca5604fce9353617fb80329bdbeff125d
Baptiste <avocalam@gmail.com>
2024-03-05 16:36:21 +0100

    add balance in $ in wallet details

--- app/wallet-details/[address]/component/BalanceSnippet.tsx
+++ app/wallet-details/[address]/component/BalanceSnippet.tsx
@@ -1,5 +1,7 @@
 import EthereumIcon from '@/components/icons/EthereumIcon';
+import { getQuotes } from '@/lib/coin_market';
 import { getBalance } from '@/lib/infura';
+import { getCurrencyValue } from '@/lib/utils';
 import { Snippet } from '@nextui-org/react';
 
 interface BalanceSnippetProps {
@@ -8,6 +10,8 @@ interface BalanceSnippetProps {
 
 export default async function BalanceSnippet({ address }: BalanceSnippetProps) {
   const balance = await getBalance(address);
+  const quotes = await getQuotes('ETH');
+  const price = quotes?.data.ETH[0]?.quote?.USD?.price || 0;
 
   return (
     <>
@@ -20,7 +24,7 @@ export default async function BalanceSnippet({ address }: BalanceSnippetProps) {
       >
         <div className="flex justify-center items-center gap-1 text-gray-600">
           <EthereumIcon className="h-4 w-4" />
-          {balance}
+          {`${balance} (${balance !== undefined && getCurrencyValue(price * balance).replace(/,/g, ' ')})`}
         </div>
       </Snippet>
     </>
