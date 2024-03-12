e3d4386ace7072f9f4c326378ae54c3eedc27c5e
Franck Fernandez <franck.fernandez@gmail.com>
2024-03-12 16:30:55 +0100

    Create Transaction type + fix getTransactions cache

--- app/api/transactions/[address]/[page]/route.ts
+++ app/api/transactions/[address]/[page]/route.ts
@@ -4,8 +4,6 @@ export async function GET(
   _: Request,
   { params: { address, page } }: { params: { address: string; page: number } }
 ) {
-  console.log(address, page);
-
   const data = await getTransactions(address, page.toString());
 
   return Response.json(data);
--- lib/etherscan.ts
+++ lib/etherscan.ts
@@ -1,7 +1,8 @@
 export async function getTransactions(address: string, page: string) {
   try {
     const response = await fetch(
-      `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=${page}&offset=20&sort=desc&apikey=${process.env.ETHERSCAN_KEY}`
+      `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=${page}&offset=20&sort=desc&apikey=${process.env.ETHERSCAN_KEY}`,
+      { next: { revalidate: 10 } }
     );
 
     if (!response.ok) {
