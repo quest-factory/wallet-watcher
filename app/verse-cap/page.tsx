1415ba4013d84a171d1f06f1cf73c3c206dae45a
Baptiste <avocalam@gmail.com>
2024-04-04 11:59:41 +0200

    add chart in page

--- app/verse-cap/page.tsx
+++ app/verse-cap/page.tsx
@@ -1,7 +1,12 @@
+import Chart from './components/Chart';
+
 export default function verseCap() {
   return (
-    <main className="w-full mx-auto px-3 md:px-0 md:max-w-[80%] my-10 space-y-12 ">
-      VERSE CAP
+    <main
+      className="w-screen border-2 border-red"
+      style={{ height: 'calc(100vh - 65px)' }}
+    >
+      <Chart />
     </main>
   );
 }
