7a66d542aac8bd80ed8b51e5953a99e793d4d3e9
Franck Fernandez <franck.fernandez@gmail.com>
2024-04-04 11:37:01 +0200

    Divide logic

--- flow/action.ts
+++ flow/action.ts
@@ -1,39 +1,65 @@
 'use server';
 
 import { createClient } from '@/lib/supabase/server';
+import { Tables } from '@/types_db';
 import { cookies } from 'next/headers';
 
-export async function createNode(state: any, formData: FormData) {
+export async function handleSubmitNode(state: any, formData: FormData) {
   const name = formData.get('name')?.toString();
-  const siren = formData.get('siren')?.toString();
-  const address = formData.get('address')?.toString();
+  const siren = formData.get('siren')?.toString() || null;
+  const address = formData.get('address')?.toString() || null;
   const edge_label = formData.get('edge_label')?.toString();
   const source = formData.get('source')?.toString();
   const target = formData.get('target')?.toString();
 
-  const supabase = createClient(cookies());
-
-  if (name) {
-    const { error: nodeError } = await supabase
-      .from('nodes')
-      .insert({ name, siren, address });
-
-    if (nodeError) {
-      console.error(nodeError.message);
-    }
-  }
-
-  if (source !== undefined || target !== undefined) {
-    const { error: edgeError } = await supabase.from('edges').insert({
-      label: edge_label,
-      source,
-      target,
+  if (name)
+    await createNode({
+      name,
+      siren,
+      address,
     });
 
-    if (edgeError) {
-      console.error(edgeError.message);
-    }
-  }
+  if (source && target) await createEdge({ label: edge_label, source, target });
 
   return state;
+}
+
+export async function createNode({
+  name,
+  siren,
+  address,
+}: {
+  name: Tables<'nodes'>['name'];
+  siren: Tables<'nodes'>['siren'];
+  address: Tables<'nodes'>['address'];
+}) {
+  const supabase = createClient(cookies());
+  const { error } = await supabase
+    .from('nodes')
+    .insert({ name, siren, address });
+
+  if (error) {
+    console.error(error.message);
+  }
+}
+
+export async function createEdge({
+  label,
+  source,
+  target,
+}: {
+  label?: Tables<'edges'>['label'];
+  source: Tables<'edges'>['source'];
+  target: Tables<'edges'>['target'];
+}) {
+  const supabase = createClient(cookies());
+  const { error } = await supabase.from('edges').insert({
+    label,
+    source,
+    target,
+  });
+
+  if (error) {
+    console.error(error.message);
+  }
 }
