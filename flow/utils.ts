b74575b9f2ca2cf112dd0aef0d56e56601d9dd3a
Franck Fernandez <franck.fernandez@gmail.com>
2024-04-10 11:27:53 +0200

    Create formatNodes and formatEdges utils

---
+++ flow/utils.ts
@@ -0,0 +1,26 @@
+import { Tables } from '@/types_db';
+import { CompanyEdge, CompanyNode } from './types';
+import { XYPosition } from 'reactflow';
+
+export const formatNodes = (dbNodes: Tables<'nodes'>[]): CompanyNode[] =>
+  dbNodes?.map(({ id, ...nodeData }) => {
+    // @ts-ignore
+    const position: XYPosition = node.position || { x: 0, y: 0 };
+    return {
+      type: 'custom',
+      id: id.toString(),
+      position,
+      data: nodeData,
+    };
+  }) || [];
+
+export const formatEdges = (dbEdges: Tables<'edges'>[]): CompanyEdge[] =>
+  dbEdges?.map(({ id, source, target, label }) => ({
+    type: 'custom',
+    id: id.toString(),
+    source,
+    target,
+    data: {
+      label,
+    },
+  })) || [];
